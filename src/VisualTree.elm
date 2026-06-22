6de7b006608a884481c74c23337b79604344d224
cactusissss <yfrny98brh@privaterelay.appleid.com>
2026-06-23 00:12:32 +0200

    Update VisualTree.elm

--- src/VisualTree.elm
+++ src/VisualTree.elm
@@ -4,7 +4,7 @@ import Html exposing (Html)
 import Tree exposing (TreeNode(..))
 import TypedSvg exposing (circle, g, line, svg, text_)
 import TypedSvg.Attributes exposing (viewBox)
-import TypedSvg.Attributes.InNamespace exposing (attr)
+import TypedSvg.Core exposing (attribute)
 import TypedSvg.Events exposing (onClick)
 
 
@@ -18,7 +18,7 @@ draw onSelect maybeTree =
             Html.div [] [ Html.text "Empty Tree" ]
 
         Just roots ->
-            svg [ viewBox 0 0 800 400 ]
+            svg [ viewBox 0 0 1200 800 ]
                 (List.indexedMap
                     (\index rootNode ->
                         renderTree onSelect rootNode (200 + toFloat index * 250) 50 1
@@ -30,10 +30,78 @@ draw onSelect maybeTree =
 renderTree : (TreeNode -> msg) -> TreeNode -> Float -> Float -> Int -> Html msg
 renderTree onSelect node xPos yPos level =
     let
-        TreeNode id label rank maybeMeta maybeChildren =
-            node
+        ( label, rank, maybeChildren ) =
+            case node of
+                TreeNode _ nodeLabel nodeRank _ nodeChildren ->
+                    ( nodeLabel, nodeRank, nodeChildren )
 
         nodeColor =
+            case String.toLower rank of
+                "family" ->
+                    "#ff6b6b"
+
+                "subfamily" ->
+                    "#4dadf7"
+
+                "genus" ->
+                    "#51cf66"
+
+                "species" ->
+                    "#fcc419"
+
+                _ ->
+                    "#adb5bd"
+
+        childrenSvg =
+            case maybeChildren of
+                Nothing ->
+                    []
+
+                Just children ->
+                    List.indexedMap
+                        (\index child ->
+                            let
+                                childX =
+                                    xPos + (toFloat index - (toFloat (List.length children - 1) / 2)) * (180 / toFloat level)
+
+                                childY =
+                                    yPos + 80
+                            in
+                            g []
+                                [ line
+                                    [ attribute "x1" (String.fromFloat xPos)
+                                    , attribute "y1" (String.fromFloat yPos)
+                                    , attribute "x2" (String.fromFloat childX)
+                                    , attribute "y2" (String.fromFloat childY)
+                                    , attribute "stroke" "#dee2e6"
+                                    ]
+                                    []
+                                , renderTree onSelect child childX childY (level + 1)
+                                ]
+                        )
+                        children
+    in
+    g []
+        ([ circle
+            [ attribute "cx" (String.fromFloat xPos)
+            , attribute "cy" (String.fromFloat yPos)
+            , attribute "r" "15"
+            , attribute "fill" nodeColor
+            , attribute "cursor" "pointer"
+            , onClick (onSelect node)
+            ]
+            []
+         , text_
+            [ attribute "x" (String.fromFloat xPos)
+            , attribute "y" (String.fromFloat (yPos - 20))
+            , attribute "font-size" "12"
+            , attribute "text-anchor" "middle"
+            , attribute "fill" "#343a40"
+            ]
+            [ Html.text label ]
+         ]
+            ++ childrenSvg
+        )
             case String.toLower rank of
                 "family" ->
                     "#ff6b6b"
