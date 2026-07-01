1b78bb263da19f9e318d99072b44ee67dafc4346
lfb241 <l-f.brske@outlook.de>
2026-07-01 12:04:47 +0200

    added url-route for Search

--- src/Route.elm
+++ src/Route.elm
@@ -1,12 +1,15 @@
 module Route exposing (Route(..), parseUrl)
 import Url exposing (Url)
-import Url.Parser exposing (Parser, (</>), top, map, oneOf, s, string, parse)
+import Url.Parser exposing (Parser, (</>),(<?>), top, map, oneOf, s, string, parse)
+import Url.Parser.Query as Query
 
 type Route
   = 
   Home
+  | Search (Maybe String)
   | Tree String
   | Node String String
+
 
 -- top ist Homepage
 routeParser : Parser (Route -> a) a
@@ -16,6 +19,7 @@ routeParser =
     [
      map Node (s "tree" </> string </> s "node" </> string)
     , map Tree (s "tree" </> string)
+    , map Search (s "search" <?> Query.string "query")
     ,  map Home top
     ]
 
@@ -26,6 +30,8 @@ parseUrl url =
         |> Maybe.withDefault Home
 
 -- Beispiele
+
 -- /TaxoView --> Just (Home)
+-- /TaxoView/search?query=test --> Just(Search "test")
 -- /TaxoView/tree/primates --> Just(Tree "primates")
 -- /tree/primates/node/Human --> Just(Node "primates" "human")
