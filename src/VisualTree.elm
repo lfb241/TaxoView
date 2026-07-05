40619393b44919bb74fee7871cd2c1ea92d98949
cactusissss <yfrny98brh@privaterelay.appleid.com>
2026-07-05 19:01:11 +0200

    Update VisualTree.elm

--- src/VisualTree.elm
+++ src/VisualTree.elm
@@ -8,7 +8,7 @@ import TypedSvg.Core exposing (attribute)
 import TypedSvg.Events exposing (onClick)
 
 -- Methode macht aus TreeNode-Liste ein HTML-Objekt
--- TODO: responsive machen
+-- SVG nutzt viewBox -> zu verfügbare Seitenbreite angepasst
 draw : (TreeNode -> msg) -> Maybe (List TreeNode) -> Html msg
 draw onSelect maybeTree =
     case maybeTree of
@@ -19,15 +19,22 @@ draw onSelect maybeTree =
             Html.div [] [ Html.text "Empty Tree" ]
 
         Just roots ->
-            svg [ viewBox 0 0 1200 800 ]
+            svg
+                [ viewBox 0 0 1200 800
+                , attribute "class" "tree-svg"
+                , attribute "preserveAspectRatio" "xMidYMin meet"
+                ]
                 (List.indexedMap
                     (\index rootNode ->
-                        renderTree onSelect rootNode (200 + toFloat index * 250) 50 1
+                        renderTree onSelect rootNode (600 + toFloat index * 300) 60 1
                     )
                     roots
                 )
 
--- Hilfsmethode zur SVG-Generierung TODO: add comments
+-- Hilfsmethode zur SVG-Generierung
+-- Ein Knoten und alle seine untergeordneten Knoten werden rekursiv dargestellt.
+-- Jeder Knoten wird als farbiger Kreis mit einer Beschriftung darüber dargestellt.
+-- Eltern-Kind-Beziehungen werden als SVG-Linien dargestellt.
 renderTree : (TreeNode -> msg) -> TreeNode -> Float -> Float -> Int -> Html msg
 renderTree onSelect node xPos yPos level =
     let
@@ -36,23 +43,8 @@ renderTree onSelect node xPos yPos level =
                 TreeNode _ nodeLabel nodeRank _ nodeChildren ->
                     ( nodeLabel, nodeRank, nodeChildren )
 
-        -- TODO: hier fehlen noch Farben für Rank = order, suborder, infraorder
         nodeColor =
-            case String.toLower rank of
-                "family" ->
-                    "#ff6b6b"
-
-                "subfamily" ->
-                    "#4dadf7"
-
-                "genus" ->
-                    "#51cf66"
-
-                "species" ->
-                    "#fcc419"
-
-                _ ->
-                    "#adb5bd"
+            colorForRank rank
 
         childrenSvg =
             case maybeChildren of
@@ -60,14 +52,23 @@ renderTree onSelect node xPos yPos level =
                     []
 
                 Just children ->
+                    let
+                        childCount =
+                            List.length children
+
+                        spacing =
+                            max 90 (280 / toFloat level)
+                    in
                     List.indexedMap
                         (\index child ->
                             let
                                 childX =
-                                    xPos + (toFloat index - (toFloat (List.length children - 1) / 2)) * (180 / toFloat level)
+                                    xPos
+                                        + (toFloat index - (toFloat (childCount - 1) / 2))
+                                        * spacing
 
                                 childY =
-                                    yPos + 80
+                                    yPos + 95
                             in
                             g []
                                 [ line
@@ -76,6 +77,7 @@ renderTree onSelect node xPos yPos level =
                                     , attribute "x2" (String.fromFloat childX)
                                     , attribute "y2" (String.fromFloat childY)
                                     , attribute "stroke" "#dee2e6"
+                                    , attribute "stroke-width" "2"
                                     ]
                                     []
                                 , renderTree onSelect child childX childY (level + 1)
@@ -87,7 +89,7 @@ renderTree onSelect node xPos yPos level =
         ([ circle
             [ attribute "cx" (String.fromFloat xPos)
             , attribute "cy" (String.fromFloat yPos)
-            , attribute "r" "15"
+            , attribute "r" "16"
             , attribute "fill" nodeColor
             , attribute "cursor" "pointer"
             , onClick (onSelect node)
@@ -96,7 +98,7 @@ renderTree onSelect node xPos yPos level =
          , text_
             [ attribute "x" (String.fromFloat xPos)
             , attribute "y" (String.fromFloat (yPos - 20))
-            , attribute "font-size" "12"
+            , attribute "font-size" "13"
             , attribute "text-anchor" "middle"
             , attribute "fill" "#343a40"
             ]
@@ -104,3 +106,31 @@ renderTree onSelect node xPos yPos level =
          ]
             ++ childrenSvg
         )
+        
+-- Weist den taxonomischen Rängen Farben zu
+colorForRank : String -> String
+colorForRank rank =
+    case String.toLower rank of
+        "order" ->
+            "#845ef7"
+
+        "suborder" ->
+            "#339af0"
+
+        "infraorder" ->
+            "#22b8cf"
+
+        "family" ->
+            "#ff6b6b"
+
+        "subfamily" ->
+            "#4dabf7"
+
+        "genus" ->
+            "#51cf66"
+
+        "species" ->
+            "#fcc419"
+
+        _ ->
+            "#adb5bd"
